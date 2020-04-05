40116535239dd9b5b8f1aa8633ecde9c12251c84
mfordos <55068027+mfordos@users.noreply.github.com>
2020-04-05 05:01:34 -0400

    Initial setup for Spotify's App Remote SDK

--- app/src/main/java/edu/uc/amplify/MainActivity.kt
+++ app/src/main/java/edu/uc/amplify/MainActivity.kt
@@ -1,10 +1,22 @@
 package edu.uc.amplify
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.spotify.android.appremote.api.ConnectionParams
+import com.spotify.android.appremote.api.Connector
+import com.spotify.android.appremote.api.SpotifyAppRemote
+import com.spotify.protocol.client.Subscription
+import com.spotify.protocol.types.PlayerState
+import com.spotify.protocol.types.Track
 import edu.uc.amplify.ui.main.MainFragment
 
+
 class MainActivity : AppCompatActivity() {
+
+    // for more information on what we're doing with the Spotify connection, see https://developer.spotify.com/documentation/android/quick-start/
+    private val CLIENT_ID = "801abd6d08334d76b320bed184ae2db4"
+    private val REDIRECT_URI = "http://edu.uc.amplify/callback"
+    private var mSpotifyAppRemote: SpotifyAppRemote? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -14,6 +26,41 @@ class MainActivity : AppCompatActivity() {
                 .replace(R.id.container, MainFragment.newInstance())
                 .commitNow()
         }
+    }
+
+    override fun onStart() {
+        super.onStart()
+        val connectionParams: ConnectionParams = Builder(CLIENT_ID)
+            .setRedirectUri(REDIRECT_URI)
+            .showAuthView(true)
+            .build()
+
+        SpotifyAppRemote.connect(this, connectionParams,
+            object : ConnectionListener() {
+                fun onConnected(spotifyAppRemote: SpotifyAppRemote) {
+                    mSpotifyAppRemote = spotifyAppRemote
+                    Log.d("MainActivity", "Connected! Yay!")
+                    // Now you can start interacting with App Remote
+                    connected()
+                }
+
+                fun onFailure(throwable: Throwable) {
+                    Log.e("MainActivity", throwable.message, throwable)
+                    // Something went wrong when attempting to connect! Handle errors here
+                }
+            })
+    }
+
+    override fun onStop() {
+        super.onStop()
+        SpotifyAppRemote.disconnect(mSpotifyAppRemote);
+    }
+
+    private fun connected() {
+        // below is just an example, but a starting point for playing a playlist
+        mSpotifyAppRemote.getPlayerApi().play("spotify:playlist:37i9dQZF1DX2sUQwD7tbmL");
+
+
     }
 
     private fun logon() {
